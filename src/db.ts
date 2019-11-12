5b51261fb30d66fcfe0e6a2e18908f0d695f0fcf
Casey Webb <notcaseywebb@gmail.com>
2019-11-12 16:49:26 -0600

    :construction: WIP

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,4 @@
-import { openDB, DBSchema } from 'idb'
+import { openDB, DBSchema, IDBPTransaction, IDBPDatabase } from 'idb'
 import { Elm } from './Main'
 
 const VERSION = 1
@@ -15,6 +15,7 @@ type ExtractElmPortData<
   ? TData
   : unknown
 
+/* Types used in DB */
 type RecipeSchema = {
   slug: string
   name: string
@@ -30,7 +31,9 @@ type IngredientSchema = {
   section: string
 }
 
+/* Types used in Elm app */
 type Recipe = ExtractElmPortData<'saveRecipe'>
+type Ingredient = IngredientSchema
 
 interface DB extends DBSchema {
   recipes: {
@@ -44,7 +47,7 @@ interface DB extends DBSchema {
   }
 }
 
-async function getDB() {
+async function getDB(): Promise<IDBPDatabase<DB>> {
   return await openDB<DB>('recipe-book', VERSION, {
     upgrade(db) {
       db.createObjectStore('recipes', { keyPath: 'slug' })
@@ -78,19 +81,14 @@ export async function listRecipes(): Promise<{
   recipes: Recipe[]
 }> {
   const db = await getDB()
-  const tx = await db.transaction(['recipes', 'ingredients'], 'readonly')
+  const tx = db.transaction(['recipes', 'ingredients'], 'readonly')
   const recipesStore = tx.objectStore('recipes')
-  const ingredientsStore = tx.objectStore('ingredients')
   const count = await recipesStore.count()
   const recipes: Recipe[] = []
   let cursor = await recipesStore.openCursor(undefined)
   while (cursor) {
-    recipes.push({
-      ...cursor.value,
-      ingredients: (await Promise.all(
-        cursor.value.ingredients.map((i) => ingredientsStore.get(i))
-      )).filter((i) => typeof i !== 'undefined') as Ingredient[]
-    })
+    const recipe = cursor.value
+    recipes.push(await _joinIngredients(recipe, tx))
     cursor = await cursor.continue()
   }
   await tx.done
@@ -99,33 +97,48 @@ export async function listRecipes(): Promise<{
 
 export async function fetchRecipeById(id: string): Promise<string | Recipe> {
   const db = await getDB()
-  const tx = await db.transaction(['recipes', 'ingredients'])
-  const recipesStore = await tx.objectStore('recipes')
-  const ingredientsStore = await tx.objectStore('ingredients')
+  const tx = db.transaction(['recipes', 'ingredients'])
+  const recipesStore = tx.objectStore('recipes')
   const recipe = await recipesStore.get(id)
   if (!recipe) return 'Recipe not found'
-  const ingredients = (await Promise.all(
-    recipe.ingredients.map((i) => ingredientsStore.get(i))
-  )).filter((i) => typeof i !== 'undefined') as Ingredient[]
   await tx.done
-  return {
-    ...recipe,
-    ingredients
-  }
+  return await _joinIngredients(recipe, tx)
 }
 
-export async function listIngredients(): Promise<string[]> {
+export async function listIngredients(): Promise<Ingredient[]> {
   const db = await getDB()
-  const tx = await db.transaction('ingredients', 'readonly')
+  const tx = db.transaction('ingredients', 'readonly')
   const ingredientsStore = tx.objectStore('ingredients')
-  const ingredients: string[] = []
+  const ingredients: Ingredient[] = []
   let cursor = await ingredientsStore
     .index('name')
     .openCursor(undefined, 'nextunique')
   while (cursor) {
-    ingredients.push(cursor.value.name)
+    ingredients.push(cursor.value)
     cursor = await cursor.continue()
   }
   await tx.done
   return ingredients
+}
+
+async function _joinIngredients(
+  recipe: RecipeSchema,
+  tx: IDBPTransaction<DB, ('recipes' | 'ingredients')[]>
+): Promise<Recipe> {
+  const ingredientsStore = tx.objectStore('ingredients')
+  const ingredients = await Promise.all(
+    recipe.ingredients.map(async (i) => {
+      const ingredient = await ingredientsStore.get(i.ingredientId)
+      if (!ingredient) throw new Error()
+      return {
+        quantity: i.quantity,
+        unit: i.unit,
+        ...ingredient
+      }
+    })
+  )
+  return {
+    ...recipe,
+    ingredients
+  }
 }
